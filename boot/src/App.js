a53da12e55bf45640cd269e005322f83a669fa01
TheoBueno <143668478+TheoBueno@users.noreply.github.com>
2023-11-19 22:13:16 -0500

    Update App.js

--- boot/src/App.js
+++ boot/src/App.js
@@ -9,9 +9,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Let's learn to integrate Bootstrap in here!
+        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
