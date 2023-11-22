8159726d599882617d267e938548eabbf9e7c0e8
TheoBueno <143668478+TheoBueno@users.noreply.github.com>
2023-11-21 23:31:39 -0500

    moved files

--- boot/src/App.js
+++ boot/src/App.js
@@ -3,23 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Let's learn to integrate Bootstrap in here!
-        </p>
-        <a
-          className="App-link"
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+
     </div>
   );
 }
